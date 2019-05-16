d67c55d1600293fe79c52841766fb93c12d2ada4
malkhoury688 <malkhoury688@CHS-312-35.chs.win.fuhsd.org>
2019-05-16 09:31:53 -0700

    added stuff to tetromino and tetrominoT a bit since they were empty

--- src/engine/Tetromino.java
+++ src/engine/Tetromino.java
@@ -1,10 +1,23 @@
 package engine;
 
+import java.util.ArrayList;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 /**
  * This class represents a Tetromino object as a set of ordered pairs, in the
  * theta = 0 rotation. It will also handle the rotational translations of these
  * ordered pairs.
  */
-public class Tetromino {
-    
+public abstract class Tetromino extends javafx.scene.image.ImageView {
+    ArrayList<ImageView> al;
+    Image image;
+    ImageView iv;
+    abstract void rotate();
+    public abstract void act(long now);
+    public void move(double x, double y) {
+        super.setX(super.getX() + x);
+        super.setY(super.getY() + y);
+    }
 }
---
+++ src/engine/TetrominoT.java
@@ -0,0 +1,40 @@
+package engine;
+
+import java.util.ArrayList;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class TetrominoT extends Tetromino {
+
+//	public TetrominoT() {
+//		image = new Image("https://t5.rbxcdn.com/0affe2ebea58132423540072cecdc010");
+//		iv.setImage(image);
+//		al = new ArrayList<ImageView>();
+//		iv.setX(50);
+//		al.add(iv);
+//		al.add(iv);
+//		al.add(iv);
+//		al.add(iv);
+//	}
+	
+	public TetrominoT() {
+		
+		super();
+		al = new ArrayList<ImageView>();
+		super.setImage(new Image("https://t5.rbxcdn.com/0affe2ebea58132423540072cecdc010"));
+		al.add(e)
+	}
+	
+	@Override
+	void rotate() {
+
+	}
+
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
